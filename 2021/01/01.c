7975e7ebb87577e32c4d0dd67264c12db474b991
Accacio <raccacio@poli.ufrj.br>
2021-12-01 21:50:50 +0100

    challenge 1

---
+++ 2021/01/01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char *argv[])
+{
+    if(!argv[1]){
+        printf("no file given\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int inputNum, lastNum, counter;
+    counter=-1;
+    FILE * file = fopen(argv[1], "r");
+    while(fscanf(file, "%d",&inputNum)!=EOF)
+    {
+        if(inputNum>lastNum) counter++;
+        lastNum=inputNum;
+    }
+    fclose(file);
+    printf("%d\n",counter);
+    return 0;
+}
