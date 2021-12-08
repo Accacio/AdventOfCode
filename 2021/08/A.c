61d91d6b7f2e27950c680a509ed6b0ce4f9afd71
Accacio <raccacio@poli.ufrj.br>
2021-12-08 20:00:44 +0100

    day 8 part 1

---
+++ 2021/08/A.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(int argc, char *argv[])
+{
+    if(!argv[1]){
+        printf("no file given\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE * file = fopen(argv[1], "r");
+    char *line;
+    fscanf(file, "%ms",&line);
+
+    int count=0;
+    while(fscanf(file, "%m[^|]s",&line)!=EOF)
+    {
+        getc(file);
+        fscanf(file, "%m[^\n]s",&line);
+        if(line)
+        {
+            char * s;
+            char * saveptr;
+            saveptr=line;
+            for (int i = 0; i < 4; i++) {
+                sscanf(saveptr, "%ms",&s);
+                switch (strlen(s)) {
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 7:
+                        count++;
+                        break;
+                    default:
+                        break;
+                }
+                saveptr+=strlen(s)+1;
+                free(s);
+            }
+        }
+        free(line);
+    }
+
+    printf("%d\n",count);
+    return 0;
+}
