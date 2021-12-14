2972d324588bd7cdf9fa3e852ed5023c286e3b78
Accacio <raccacio@poli.ufrj.br>
2021-12-14 11:27:31 +0100

    day 13 part 1

--- 2021/13/A.c
+++ 2021/13/A.c
@@ -16,11 +16,6 @@ init_paper(Paper * paper,int rows, int cols)
     paper->cols = cols;
     paper->map = (int*) malloc(rows*cols*sizeof(int));
     memset(paper->map,0,rows*cols*sizeof(int));
-    /* for (int i = 0; i < rows; i++) { */
-    /*     for (int j = 0; j < cols; j++) { */
-    /*         *(paper->map+i+j*cols) = 0; */
-    /*     } */
-    /* } */
 }
 
 void
@@ -29,9 +24,9 @@ print_paper(Paper paper)
     for (int i = 0; i < paper.rows; i++) {
         for (int j = 0; j < paper.cols; j++) {
             if(*(paper.map+j+i*paper.cols))
-            printf("#");
+                printf("#");
             else
-            printf(".");
+                printf(".");
         }
         printf("\n");
     }
@@ -54,23 +49,18 @@ count_paper(Paper paper)
 void
 fold_paper_vert(Paper * paper,int row)
 {
-    long ret=0;
     for (int i = row; i < paper->rows; i++) {
         for (int j = 0; j < paper->cols; j++) {
             int * cur=paper->map+j+i*paper->cols;
             if(*cur){
-            int corresponding_row=row-(i-row);
-            *(paper->map+corresponding_row*paper->cols+j)=1;
+                int corresponding_row=row-(i-row);
+                *(paper->map+corresponding_row*paper->cols+j)=1;
             }
-            /* printf("%d",*cur); */
         }
-            /* printf("\n"); */
     }
     int oldrows=paper->rows;
     paper->rows = row;
     paper->map = realloc(paper->map,row*paper->cols*sizeof(int));
-    /* memset(paper->map+oldrows*paper->cols,0,()*sizeof(int)); */
-    return ret;
 }
 
 void
@@ -78,13 +68,14 @@ fold_paper_horz(Paper * paper,int col)
 {
     long ret=0;
     int * init=paper->map;
-    paper->map = malloc(col*paper->rows*sizeof(int));
-    memset(paper->map,0,col*paper->rows*sizeof(int));
+    int newcols=col;
+    paper->map = malloc(newcols*paper->rows*sizeof(int));
+    memset(paper->map,0,newcols*paper->rows*sizeof(int));
 
     for (int i = 0; i < paper->rows; i++) {
-        for (int j = 0; j < col+1; j++) {
+        for (int j = 0; j < newcols; j++) {
             int * cur=init+j+i*paper->cols;
-            int * new=paper->map+j+i*col;
+            int * new=paper->map+j+i*newcols;
             *new=*cur;
         }
     }
@@ -93,45 +84,44 @@ fold_paper_horz(Paper * paper,int col)
             int * cur=init+j+i*paper->cols;
             int * new=paper->map+j+i*paper->cols;
             if(*cur){
-            int corresponding_col=col-(j-col);
-            *(paper->map+corresponding_col+i*col)=1;
+                int corresponding_col=col-(j-col);
+                *(paper->map+corresponding_col+i*col)=1;
             }
-            /* printf("%d",*cur); */
         }
-            /* printf("\n"); */
     }
     paper->cols = col;
     free(init);
-    return ret;
 }
 
 
 void
-paper_add_value(Paper * paper,int x,int y)
+paper_add_value(Paper * paper,int col,int row)
 {
-    if(x>=paper->cols)
+    if(col>=paper->cols)
     {
         int * init=paper->map;
-        paper->map = malloc((x+1)*paper->rows*sizeof(int));
-        memset(paper->map,0,(x+1)*paper->rows*sizeof(int));
+        int newcols=col+1;
+        paper->map = malloc((newcols+1)*paper->rows*sizeof(int));
+        memset(paper->map,0,(newcols+1)*paper->rows*sizeof(int));
 
         for (int i = 0; i < paper->rows; i++) {
             for (int j = 0; j < paper->cols; j++) {
                 int * cur=init+j+i*paper->cols;
-                int * new=paper->map+j+i*(x+1);
+                int * new=paper->map+j+i*(newcols);
                 *new=*cur;
             }
         }
-        paper->cols=x+1;
+        paper->cols=newcols;
         free(init);
     }
 
-    if(y>paper->rows)
+    if(row>=paper->rows)
     {
-        paper->map=realloc(paper->map,(y+1)*paper->cols*sizeof(int));
-        paper->rows=y+1;
+        paper->map=realloc(paper->map,(row+1)*paper->cols*sizeof(int));
+        memset(paper->map+paper->rows*paper->cols,0,(row+1-paper->rows)*paper->cols*sizeof(int));
+        paper->rows=row+1;
     }
-    *(paper->map+x+y*paper->cols) = 1;
+    *(paper->map+col+row*paper->cols) = 1;
 }
 
 int
@@ -147,64 +137,30 @@ main(int argc, char *argv[])
 
     Paper paper;
     init_paper(&paper,1,1);
-    /* init_paper(&paper,10,10); */
-
-    /* print_paper(paper); */
-    /* int x,y; */
-    /* while(fscanf(file,"%d",&x)) */
-    /* { */
-    /*     getc(file); */
-    /*     fscanf(file,"%d",&y); */
-    /*     printf("%d %d\n",x,y); */
-    /*     *(paper.map+x+y*paper.cols) = 1; */
-
-    /* } */
-    /* fscanf(file,"%d",&x) */
-    /*             getc(file); */
-    /*     fscanf(file,"%d",&y); */
-    /*     printf("%d %d\n",x,y); */
-
-
-    while(fscanf(file, "%ms",&line)&&strcmp(line,"fold")!=0)
+    while(fscanf(file, "%ms",&line)!=EOF)
     {
+        if(line)
+        {
 
-        /* if(!line) break; */
-        int x;
-        int y;
-        char * saveptr;
-        char * token = strtok_r(line, ",",&saveptr);
-        x=atoi(token);
-        token = strtok_r(NULL, ",",&saveptr);
-
-
-        /* printf("%d %d\n",paper.rows, paper.cols); */
-        /* token = strtok(NULL, ","); */
-        y=atoi(token);
-        /* free(line); */
-        /* *(paper.map+x+y*paper.cols) = 1; */
-
-        paper_add_value(&paper,x,y);
-        printf("Size %d %d\n",paper.rows, paper.cols);
-        printf("%d %d\n",x,y);
-        free(line);
+            char*saveptr = line;
+            char * xstr=strsep(&saveptr, ",");
+            char * ystr=strsep(&saveptr, ",");
+            if(!xstr||!ystr) break;
+            paper_add_value(&paper,atoi(xstr),atoi(ystr));
+            free(line);
+        }
     }
-    print_paper(paper);
+    fscanf(file, "%ms",&line);
+    free(line);
+    fscanf(file, "%ms",&line);
+    char*saveptr = line;
+    char * dir=strsep(&saveptr, "=");
+    char * size=strsep(&saveptr, "=");
+    if(strcmp(dir,"x")==0) fold_paper_horz(&paper,atoi(size));
+    else fold_paper_vert(&paper,atoi(size));
+    free(line);
 
-
-        printf("\n");
-    fold_paper_vert(&paper,7);
-    fold_paper_horz(&paper,5);
-        printf("\n");
-        print_paper(paper);
-    /* fold_paper_horz(&paper,5); */
-    /* fscanf(file, "%ms",&line); */
-
-        /* printf("\n"); */
-        /* print_paper(paper); */
-        printf("\n");
-        printf("%ld\n",count_paper(paper));
-
-    /* free(line); */
+    printf("%ld\n",count_paper(paper));
 
     return 0;
 }
